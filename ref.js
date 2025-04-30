a588d8c62977a7012375be447e78c968675967ac
Stijn Holtus <stijn@powerkraut.nl>
2025-04-30 09:49:05 +0200

    fix?

--- ref.js
+++ ref.js
@@ -3,7 +3,7 @@ const pk = require("./index");
 function ref(name) {
     let sources = pk.getSources();
     for(let s in sources) {
-        if(s.name == name){
+        if(sources[s].name == name){
             return "`" + sources[s].database + "." + sources[s].schema + "." + sources[s].name + "` "
         }
     }
