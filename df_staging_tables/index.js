a67249f57711fdbdf26a389fc4f7a2956112f3a4
Stijn Holtus <stijn@powerkraut.nl>
2025-04-30 11:39:06 +0200

    added: partitioning

--- df_staging_tables/index.js
+++ df_staging_tables/index.js
@@ -6,7 +6,13 @@ function stg_ga4_events_sessies () {
         "config": {
             "type": "table",
             "schema": "df_staging_tables",
-            "dependencies": require('./stg_ga4_events_sessies').refs
+            "dependencies": require('./stg_ga4_events_sessies').refs,
+            "uniqueKey": ["unique_event_id"],
+            "bigquery": {
+                "partitionBy": "event_date",
+                "updatePartitionFilter":
+                    "event_date >= date_sub(current_date(), interval 1 day)"
+            }
         },
         "query": require('./stg_ga4_events_sessies').query
     }
