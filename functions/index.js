76e30bdebf8c836ac421c98252d92e60d6cc5463
Stijn Holtus <stijn@powerkraut.nl>
2025-05-08 16:05:32 +0200

    FIX: alias

--- functions/index.js
+++ functions/index.js
@@ -9,9 +9,9 @@ function ref(p1, p2) {
     let NrFound = 0;
     for(let s in sources) {
         if( //if the ref has only one parameter it has to be the name, when there are 2 parameter the second wil be the name. (name is interchangable with alias)
-            (typeof p2 == "undefined" && (sources[s].name === p1 || (sources[s].alias === p1 && typeof sources[s].name == 'undefined') ) )
+            (typeof p2 == "undefined" && (sources[s].alias === p1 || (sources[s].name === p1 && typeof sources[s].alias == 'undefined') ) )
             ||
-            (typeof p2 != "undefined" && (sources[s].name === p2 || (sources[s].alias === p2 && typeof sources[s].name == 'undefined') ) && sources[s].schema === p1)
+            (typeof p2 != "undefined" && (sources[s].alias === p2 || (sources[s].name === p2 && typeof sources[s].alias == 'undefined') ) && sources[s].schema === p1)
         ){
             ref[NrFound] = "`" + sources[s].database + "." + sources[s].schema
             //voeg een suffix voor development toe. Alleen toevoegen als het niet om brondata gaat (gedefineerd als rawdata of googleSheets)
