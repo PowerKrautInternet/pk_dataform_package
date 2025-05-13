55178b613207595e1fcfead75577ed7cb5b91427
Stijn Holtus <stijn@powerkraut.nl>
2025-05-13 12:24:39 +0200

    try: fix not query error 7

--- setup/index.js
+++ setup/index.js
@@ -52,7 +52,7 @@ function setupFunctions(sources){
     query[0] = getLookup();
     for(let s in sources){
         if(sources[s].name.endsWith("DataProducer")){
-            query[s+1] = lasttransaction(sources[s].name);
+            query.push(lasttransaction(sources[s].name));
         }
     }
     return query
