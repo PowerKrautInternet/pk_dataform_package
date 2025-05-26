94111e809e43b269a65d1384fb405cc2c04dc0d2
Stijn Holtus <stijn@powerkraut.nl>
2025-05-26 10:27:07 +0200

    GA4 dk_monitor

--- queries/df_datakwaliteit/index.js
+++ queries/df_datakwaliteit/index.js
@@ -135,6 +135,7 @@ function dk_monitor(){
             if(sources[s].key1 == undefined || name === "events_*"){
                 query += "MAX("
             }
+
             query += "stats.KEY1"
             if(sources[s].key1 == undefined || name === "events_*"){
                 query += ") as KEY1"
