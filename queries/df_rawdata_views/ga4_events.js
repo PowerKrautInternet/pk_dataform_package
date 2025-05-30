1e35676818c25b039c749162e860e2fe2da660c3
Stijn Holtus <stijn@powerkraut.nl>
2025-05-30 15:21:04 +0200

    Debug

--- queries/df_rawdata_views/ga4_events.js
+++ queries/df_rawdata_views/ga4_events.js
@@ -118,5 +118,5 @@ FROM ${ref("events_*")}
 
 `
 let refs = pk.getRefs()
-for (let r of refs) {query += "--" + r.name}
+for (let r of refs) {query += "--" + r.valueAsString}
 module.exports = {query, refs}
