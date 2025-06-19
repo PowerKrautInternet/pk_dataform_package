f5aa78794218b8d8ca6ee977f9bf4047f11014dd
Stijn Holtus <stijn@powerkraut.nl>
2025-06-19 21:24:10 +0200

    Fix

--- queries/df_staging_views/stg_ga4_marketing_kanalen_combined.js
+++ queries/df_staging_views/stg_ga4_marketing_kanalen_combined.js
@@ -3,7 +3,7 @@ const {join, ref, getRefs, ifSource, ifNull} = require("../../sources");
 let query = `
 
 SELECT
-    ga4.* EXCEPT(bron, kanaal, session_campaign, event_date, session_campaign_id, session_google_ads_ad_group_id, session_google_ads_ad_group_name),
+    ga4.* EXCEPT(bron, kanaal, session_campaign, event_date, session_campaign_id, session_google_ads_ad_group_id, session_google_ads_ad_group_name, event_name),
     ${ifSource("stg_marketingkanalen_combined", "marketing_kanalen.* EXCEPT(bron, campaign_name, record_date, campaign_id, ad_group_id, ad_group_name, merk),")}
     ${ifNull([
         "ga4.bron",
