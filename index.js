cfa6b60c14a3717c92138e640202e039198167dc
Stijn Holtus <stijn@powerkraut.nl>
2025-06-25 13:44:58 +0200

    Fix

--- index.js
+++ index.js
@@ -2,4 +2,29 @@ function table (publishFunction, endTable, config, query) {
     publishFunction(endTable, config).query(query)
 }
 
-module.exports = {table}
+let config = {type: "view"}
+
+class viewClass {
+    constructor () {
+        this.select_block = true
+        this.from_block = true
+        this.viewQuery = {select: "*", from: "`anywhere`"}
+    };
+    select (enable) {
+        this.select_block = enable
+        return this
+    }
+    query (query) {
+        let temp = "";
+        temp += this.select_block ? this.viewQuery.select : "";
+        temp += this.from_block ? this.viewQuery.from : "";
+        return temp
+
+    }
+}
+
+function view () {
+    return new viewClass()
+}
+
+module.exports = {table, config, view}
