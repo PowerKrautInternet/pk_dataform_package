977a5a36aff291c9a7eb1121e91fcaf1bd67ea27
PowerKrautWouterFloors <138665492+PowerKrautWouterFloors@users.noreply.github.com>
2025-07-09 12:17:13 +0200

    Create bing_assetgroup_performance.js
    Added assetgroup performance

---
+++ queries/df_rawdata_views/bing_assetgroup_performance.js
@@ -0,0 +1,34 @@
+
+/*config*/
+let pk = require("../../sources")
+let ref = pk.ref
+let query = `
+
+SELECT  
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.pk_crm_id') AS pk_crm_id,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.type') AS type,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.AccountId') AS AccountId,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.AccountName') AS AccountName,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.CampaignId') AS CampaignId,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.CampaignName') AS CampaignName,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.TimePeriod') AS TimePeriod,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.AssetGroupId') AS AssetGroupId,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.AccountStatus') AS AccountStatus,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.AssetGroupName') AS AssetGroupName,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.AssetGroupStatus') AS AssetGroupStatus,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.AverageCpc') AS AverageCpc,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.CampaignStatus') AS CampaignStatus,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Clicks') AS Clicks,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Conversions') AS Conversions,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Ctr') AS Ctr,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Impressions') AS Impressions,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.ReturnOnAdSpend') AS ReturnOnAdSpend,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Revenue') AS Revenue,
+  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Spend') AS Spend
+
+FROM `pk-datalake-stellantis.df_rawdata_views_dev.bing_dataproducer_lasttransaction`
+WHERE JSON_EXTRACT_SCALAR(PAYLOAD, '$.type') = 'AssetGroupPerformanceReportPublisher'
+    
+`
+let refs = pk.getRefs()
+module.exports = {query, refs}
