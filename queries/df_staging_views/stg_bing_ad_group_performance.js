16479fe01800ac2da9cd3f755ac3993b6fb0f0cd
PowerKrautWouterFloors <138665492+PowerKrautWouterFloors@users.noreply.github.com>
2025-07-10 13:41:46 +0200

    Update stg_bing_ad_group_performance.js
    edit: snake case

--- queries/df_staging_views/stg_bing_ad_group_performance.js
+++ queries/df_staging_views/stg_bing_ad_group_performance.js
@@ -4,27 +4,27 @@ let ref = pk.ref
 let query = `
 
     SELECT
-        'Microsoft Ads' as bron,
+        'Microsoft Ads' AS bron,
         IFNULL(adgroup.account, pf.account) AS account,
         IFNULL(adgroup.pk_crm_id, pf.pk_crm_id) AS pk_crm_id,
-        IFNULL(account_id, AccountId) AS account_id,
-        IFNULL(campaign_id, CampaignId) AS campaign_id,
-        IFNULL(adgroup_id, AssetGroupId) AS adgroup_id,
+        IFNULL(account_id, account_id) AS account_id,
+        IFNULL(campaign_id, campaign_id) AS campaign_id,
+        IFNULL(adgroup_id, asset_group_id) AS adgroup_id,
         device_type,
-        IFNULL(time_period, TimePeriod) AS time_period,
-        MAX(IFNULL(account_name, AccountName)) AS account_name,
+        IFNULL(time_period, time_period) AS time_period,
+        MAX(IFNULL(account_name, account_name)) AS account_name,
         MAX(account_number) AS account_number,
         MAX(ad_distribution) AS ad_distribution,
-        MAX(IFNULL(account_status, AccountStatus)) AS account_status,
-        MAX(IFNULL(campaign_name, CampaignName)) AS campaign_name,
-        MAX(IFNULL(campaign_status, CampaignStatus)) AS campaign_status,
+        MAX(IFNULL(account_status, account_status)) AS account_status,
+        MAX(IFNULL(campaign_name, campaign_name)) AS campaign_name,
+        MAX(IFNULL(campaign_status, campaign_status)) AS campaign_status,
         MAX(IFNULL(campaign_type, "Performance Max")) AS campaign_type,
         MAX(customer_id) AS customer_id,
         MAX(customer_name) AS customer_name,
-        MAX(IFNULL(adgroup_name, AssetGroupName)) AS adgroup_name,
+        MAX(IFNULL(adgroup_name, asset_group_name)) AS adgroup_name,
         MAX(IFNULL(adgroup_type, "Performance Max")) AS adgroup_type,
         MAX(adgroup_labels) AS adgroup_labels,
-        MAX(IFNULL(adgroup_status, AssetGroupStatus)) AS adgroup_status,
+        MAX(IFNULL(adgroup_status, asset_group_status)) AS adgroup_status,
         SUM(CAST(IFNULL(adgroup.conversions, pf.conversions) AS INT64)) AS conversions,
         SUM(CAST(conversions_qualified AS FLOAT64)) AS conversions_qualified,
         SUM(CAST(all_conversions AS INT64)) AS all_conversions,
@@ -32,7 +32,7 @@ let query = `
         SUM(CAST(IFNULL(adgroup.clicks, pf.clicks) AS INT64)) AS clicks,
         SUM(CAST(IFNULL(adgroup.spend, pf.spend) AS FLOAT64)) AS spend,
         MAX(quality_score) AS quality_score
-    
+
     FROM ${ref("df_rawdata_views", "bing_ad_group_performance")} adgroup
     
     FULL OUTER JOIN ${ref("df_rawdata_views", "bing_assetgroup_performance")} pf
