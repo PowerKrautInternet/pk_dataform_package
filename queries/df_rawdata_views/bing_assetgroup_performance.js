3c6552541fb9a7c09e1a0c7252c3a4b5d2217608
PowerKrautWouterFloors <138665492+PowerKrautWouterFloors@users.noreply.github.com>
2025-07-10 13:39:48 +0200

    Update bing_assetgroup_performance.js
    edit snake case

--- queries/df_rawdata_views/bing_assetgroup_performance.js
+++ queries/df_rawdata_views/bing_assetgroup_performance.js
@@ -5,27 +5,27 @@ let ref = pk.ref
 let query = `
 
 SELECT  
-  account,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.pk_crm_id') AS pk_crm_id,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.type') AS type,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.AccountId') AS AccountId,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.AccountName') AS AccountName,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.CampaignId') AS CampaignId,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.CampaignName') AS CampaignName,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.TimePeriod') AS TimePeriod,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.AssetGroupId') AS AssetGroupId,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.AccountStatus') AS AccountStatus,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.AssetGroupName') AS AssetGroupName,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.AssetGroupStatus') AS AssetGroupStatus,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.AverageCpc') AS AverageCpc,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.CampaignStatus') AS CampaignStatus,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Clicks') AS Clicks,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Conversions') AS Conversions,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Ctr') AS Ctr,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Impressions') AS Impressions,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.ReturnOnAdSpend') AS ReturnOnAdSpend,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Revenue') AS Revenue,
-  JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Spend') AS Spend
+account,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.pk_crm_id') AS pk_crm_id,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.type') AS type,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.AccountId') AS account_id,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.AccountName') AS account_name,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.CampaignId') AS campaign_id,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.CampaignName') AS campaign_name,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.TimePeriod') AS time_period,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.AssetGroupId') AS asset_group_id,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.AccountStatus') AS account_status,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.AssetGroupName') AS asset_group_name,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.AssetGroupStatus') AS asset_group_status,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.AverageCpc') AS average_cpc,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.CampaignStatus') AS campaign_status,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Clicks') AS clicks,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Conversions') AS conversions,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Ctr') AS ctr,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Impressions') AS impressions,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.ReturnOnAdSpend') AS return_on_ad_spend,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Revenue') AS revenue,
+JSON_EXTRACT_SCALAR(PAYLOAD, '$.response.Spend') AS spend
 
 FROM ${ref("bingAdsDataProducer_lasttransaction")}
 WHERE JSON_EXTRACT_SCALAR(PAYLOAD, '$.type') = 'AssetGroupPerformanceReportPublisher'
