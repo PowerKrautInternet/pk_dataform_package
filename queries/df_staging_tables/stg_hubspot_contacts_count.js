cabe3ab846190c51ec9338b142208bb3ba1ec0da
Stijn Holtus <stijn@powerkraut.nl>
2025-07-20 21:06:48 +0200

    stg_hubspot_contacts_count.js update: column name count

--- queries/df_staging_tables/stg_hubspot_contacts_count.js
+++ queries/df_staging_tables/stg_hubspot_contacts_count.js
@@ -3,7 +3,7 @@ let pk = require("../../sources")
 let ref = pk.ref
 let query = `
 
-    SELECT count(distinct json_value(payload, '$.email')), current_date() as receivedon FROM ${ref("rawdata","hubspotExporterDataProducer")} where json_value(payload, '$.type') = 'HubspotContactsExporterPublisher'
+    SELECT count(distinct json_value(payload, '$.email')) as count, current_date() as receivedon FROM ${ref("rawdata","hubspotExporterDataProducer")} where json_value(payload, '$.type') = 'HubspotContactsExporterPublisher'
 
 `
 let refs = pk.getRefs()
