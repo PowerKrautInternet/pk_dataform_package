83d2f46bc841651ec2d7e4b4c3adc3c7d1adbf20
Stijn Holtus <stijn@powerkraut.nl>
2025-07-20 11:57:30 +0200

    stg_hubspot_workflowstats.js updated: typo

--- queries/df_staging_views/stg_hubspot_workflowstats.js
+++ queries/df_staging_views/stg_hubspot_workflowstats.js
@@ -94,7 +94,7 @@ FROM (
 
     FROM ${ref("df_datamart_views", "dm_hubspot_emailstats")} email 
         ${join("FULL OUTER JOIN", "df_rawdata_views", "hubspot_bigquerylogging", "AS flows ON 1=0")} 
-        ${join("FULL OUTER JOIN", "df_rawdata_tables", "stg_hubspot_contact_count", "AS aantal ON 1=0")}
+        ${join("FULL OUTER JOIN", "df_rawdata_tables", "stg_hubspot_contacts_count", "AS aantal ON 1=0")}
 ) hubspot
 ${join("LEFT JOIN", "googleSheets", "gs_mapping_edmworkflow_campagne", `AS campagne ON hubspot.hs_campaign_name = campagne.edm ${ifSource("hubspot_bigquerylogging", "OR hubspot.hs_workflow_name = campagne.workflow")}`)}
 ${join("LEFT JOIN", "googleSheets", "gs_mapping_edmworkflow", "AS mapping ON hubspot.hs_campaign_name = mapping.edm")}
