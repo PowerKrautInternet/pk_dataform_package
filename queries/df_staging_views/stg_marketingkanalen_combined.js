4ac378969a55bdaf521dc500a926bbf0caf395de
PowerKrautInternet <132550480+PowerKrautInternet@users.noreply.github.com>
2025-08-06 11:45:42 +0200

    Update stg_marketingkanalen_combined.js

--- queries/df_staging_views/stg_marketingkanalen_combined.js
+++ queries/df_staging_views/stg_marketingkanalen_combined.js
@@ -7,7 +7,7 @@ SELECT
    ${ifNull([
         ifSource("stg_handmatige_uitgaves_pivot", "uitgave_merk"),
         `${ref("lookupTable")}(campaign_name, TO_JSON_STRING(ARRAY(SELECT merk FROM ${ref("gs_merken")})))`
-    ], "as merk_event,")}
+    ], "as merk,")}
 FROM (
     SELECT
         ${ifNull(['google_ads.bron', ifSource('stg_facebookdata','facebook.bron'), ifSource('dv360_data','dv360.bron'), ifSource('stg_bing_ad_group_performance','microsoft.bron'), ifSource('stg_linkedin_ads_combined','linkedin.bron'), ifSource('handmatige_uitgaves_pivot', 'handmatig.uitgave_bron')])} as bron,
