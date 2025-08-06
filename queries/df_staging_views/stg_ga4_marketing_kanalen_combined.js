0f289915f1a70bb9c301ffbca151932292e88472
PowerKrautInternet <132550480+PowerKrautInternet@users.noreply.github.com>
2025-08-06 08:32:41 +0200

    Update stg_ga4_marketing_kanalen_combined.js

--- queries/df_staging_views/stg_ga4_marketing_kanalen_combined.js
+++ queries/df_staging_views/stg_ga4_marketing_kanalen_combined.js
@@ -147,7 +147,7 @@ SELECT
     ${ifSource("stg_lef_leads_agg","lef.gewenstBrandstof AS lef_brandstof,")}
     ${ifSource("stg_lef_leads_agg","lef.gewenstBouwjaar AS lef_bouwjaar,")}
     ${ifSource("stg_hubspot_workflowstats", "hs.* EXCEPT(hs_date, hs_campaign, hs_bron),")}
-    ${ifNull([ifSource("handmatige_uitgaves_pivot", "marketing_kanalen.uitgave_categorie,"), ifSource("handmatige_uitgaves_pivot","lef.uitgave_categorie AS uitgave_categorie")])}
+    ${ifNull([ifSource("handmatige_uitgaves_pivot", "marketing_kanalen.uitgave_categorie,"), ifSource("gs_kostenlefmapping","lef.uitgave_categorie AS uitgave_categorie")])}
 
 FROM (SELECT 'GA4' as bron, * FROM ${ref("df_staging_views", "stg_ga4_mappings_targets")}) ga4
     
