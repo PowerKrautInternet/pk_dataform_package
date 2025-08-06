ef94ed35b820af01410ebad7649cb03dc3a85200
Stijn Holtus <stijn@powerkraut.nl>
2025-08-06 09:25:14 +0200

    Update stg_lef_leads_agg.js
    ')' stond verkeerd

--- queries/df_staging_views/stg_lef_leads_agg.js
+++ queries/df_staging_views/stg_lef_leads_agg.js
@@ -7,7 +7,7 @@ SELECT
 * EXCEPT(sessie_conversie_bron, kanaal, lead_rank, event_timestamp ${ifSource('gs_kostenlefmapping', ',lef_bron, lef_kwalificatie, lef_systeem, uitgave_bron, uitgave_merk')} ),
 IFNULL(sessie_conversie_bron, kanaal) AS kanaal,
 ${ifNull(['merk_session', 'gewenstMerk', ifSource('gs_kostenlefmapping', 'uitgave_merk')])} AS merk_session,
-${ifSource('gs_kostenlefmapping', ifNull(['uitgave_categorie', 'CASE WHEN leadType = "Aftersales" THEN "Aftersales" WHEN leadType = "Sales" AND gewenstAutoSoort = "Occasion" THEN "Verkoop occasion" WHEN leadType = "Sales" AND gewenstAutoSoort = "Nieuw" THEN "Verkoop Nieuw" WHEN soortLead = "Private lease" THEN "Private lease" ELSE NULL END'])'AS uitgave_categorie' )} 
+${ifSource('gs_kostenlefmapping', ifNull(['uitgave_categorie', 'CASE WHEN leadType = "Aftersales" THEN "Aftersales" WHEN leadType = "Sales" AND gewenstAutoSoort = "Occasion" THEN "Verkoop occasion" WHEN leadType = "Sales" AND gewenstAutoSoort = "Nieuw" THEN "Verkoop Nieuw" WHEN soortLead = "Private lease" THEN "Private lease" ELSE NULL END'], 'AS uitgave_categorie'))} 
                                                                      
 FROM(
   SELECT
