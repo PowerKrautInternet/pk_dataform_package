95159f50f1704cd6dea13374c682576b2d9c71e1
Stijn Holtus <stijn@powerkraut.nl>
2025-08-13 14:16:22 +0200

    Update getTelefoon_function.js

--- setup/getTelefoon_function.js
+++ setup/getTelefoon_function.js
@@ -1,3 +1,9 @@
-let string = ""
+let string = `
 
+function getTelefoon(obj) {
+ let contact = JSON.parse(obj).slice().filter((item, index) => { return item.contactwijzeType == "Telefoon" }).pop();
+ return ((contact) ? contact.value : null)
+}
+
+`
 module.exports = {string}
