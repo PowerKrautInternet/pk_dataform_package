4f44c39225d908f512191ab2f42bed6679104cb9
PowerKrautWouterFloors <138665492+PowerKrautWouterFloors@users.noreply.github.com>
2025-09-03 08:53:40 +0200

    Create sam_trajectextern.js

---
+++ queries/df_rawdata_views/sam_trajectextern.js
@@ -0,0 +1,26 @@
+
+/*config*/
+let pk = require("../../sources")
+let ref = pk.ref
+let query = `
+SELECT
+        JSON_EXTRACT_SCALAR(PAYLOAD, '$.TRAJECTEXTERNGUID') as TRAJECTEXTERNGUID,
+        JSON_EXTRACT_SCALAR(PAYLOAD, '$.TRAJECTID') as TRAJECTEXTERN_TRAJECTID,
+        JSON_EXTRACT_SCALAR(PAYLOAD, '$.SOORTLEADSBRONID') as TRAJECTEXTERN_SOORTLEADSBRONID,
+        JSON_EXTRACT_SCALAR(PAYLOAD, '$.EXTERNGUID') as TRAJECTEXTERN_EXTERNGUID,
+        JSON_EXTRACT_SCALAR(PAYLOAD, '$.DTCMEDIA_CRM_ID') as TRAJECTEXTERN_DTCMEDIA_CRM_ID
+FROM (
+  SELECT MAX(SCHEMA), PRIMARYFIELDHASH, MAX(PAYLOAD) as PAYLOAD, MAX(ACTION) as ACTION, MAX(RECEIVEDON) as RECEIVEDON
+    FROM
+       ${ref("df_rawdata_views", "samDataProducer_lasttransaction")} as first
+       WHERE SCHEMA IN (SELECT schema FROM ${ref("df_rawdata_views", "samtablehashes")}  WHERE tableName = 'TRAJECTEXTERNGU')
+       AND RECEIVEDON = (
+          SELECT MAX(RECEIVEDON)
+          FROM ${ref("df_rawdata_views", "samDataProducer_lasttransaction")} as second
+          WHERE first.PRIMARYFIELDHASH = second.PRIMARYFIELDHASH and first.SCHEMA = second.SCHEMA)
+      GROUP BY PRIMARYFIELDHASH
+)
+
+`
+let refs = pk.getRefs()
+module.exports = {query, refs}
