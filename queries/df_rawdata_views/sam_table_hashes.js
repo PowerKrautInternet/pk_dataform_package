ac65bddda1699a56e09b37d296fd30f8458fc0ae
PowerKrautWouterFloors <138665492+PowerKrautWouterFloors@users.noreply.github.com>
2025-09-03 13:37:58 +0200

    Update sam_table_hashes.js

--- queries/df_rawdata_views/sam_table_hashes.js
+++ queries/df_rawdata_views/sam_table_hashes.js
@@ -4,7 +4,7 @@ let pk = require("../../sources")
 let ref = pk.ref
 let query = `
 SELECT tableName, schema FROM (
-  SELECT *, \`pk-datalake-stam\`.rawdata.getTableName(PAYLOAD) as tableName FROM (
+  SELECT *, ${ref("getTableName")}(PAYLOAD) as tableName FROM (
     SELECT DISTINCT(schema) as SCHEMA ,MAX(PAYLOAD) as PAYLOAD FROM ${ref("samDataProducer")} GROUP BY schema
   )
 ) GROUP BY tableName, schema
