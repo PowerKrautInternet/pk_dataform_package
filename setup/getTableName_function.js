d66f9dbd4a8efec114c8f6fc8c8e7f6d513ddd34
stijn <stijn@powerkraut.nl>
2025-09-03 13:40:55 +0200

    getTableName_function.js fix
    Signed-off-by: stijn <stijn@powerkraut.nl>

--- setup/getTableName_function.js
+++ setup/getTableName_function.js
@@ -2,7 +2,6 @@ let string = `
 function getTableName(obj) {
     let contact = Object.keys(JSON.parse(obj))[0];
     return contact.replace(/ID$/ig, '');
-}
-return getTableName(json_row);`
+}`
 
 module.exports = {string}
