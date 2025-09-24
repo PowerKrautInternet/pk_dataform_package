f2d7d4b3d57f17e985336e4e81c5cc94d0bb4cac
PowerKrautWouterFloors <138665492+PowerKrautWouterFloors@users.noreply.github.com>
2025-09-24 14:36:47 +0200

    Update stg_ga4_events_sessies.js

--- queries/df_staging_views/stg_ga4_events_sessies.js
+++ queries/df_staging_views/stg_ga4_events_sessies.js
@@ -136,6 +136,9 @@ FROM(
     IFNULL(events.event_buy_item_id, sessie_assignment.event_buy_item_id) AS event_buy_item_id,
     IFNULL(events.event_trade_in_bouwjaar, sessie_assignment.event_trade_in_bouwjaar) AS event_trade_in_bouwjaar,
     IFNULL(events.event_trade_in_brandstof, sessie_assignment.event_trade_in_brandstof) AS event_trade_in_brandstof,
+    ${ifSource("stg_ga4_dealerevents", `IFNULL(events.dealer_sessie, sessie_assignment.dealer_sessie) AS dealer_sessie,
+    IFNULL(events.dealer_event, sessie_assignment.dealer_event) AS dealer_event,
+    IFNULL(events.email, sessie_assignment.email) AS email`)}
 
 
     FROM ${ref("ga4_events")} events
