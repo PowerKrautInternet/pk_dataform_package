da3ac22c94a21b0f9267371979d6b8eb4c5c3c40
Stijn Holtus <stijn@powerkraut.nl>
2025-10-14 22:28:53 +0200

    fix crm config syntec
    Signed-off-by: Stijn Holtus <stijn@powerkraut.nl>

--- sources/index.js
+++ sources/index.js
@@ -79,6 +79,7 @@ function ref(p1, p2, ifSource) {
     let NrFound = 0;
     for(let s in sources) {
         if( //if the ref has only one parameter it has to be the name, when there are 2 parameter the second wil be the name. (name is interchangable with alias)
+            typeof sources[s].name != 'undefined' &&
             (p2 === "" && (sources[s].alias === p1 || (sources[s].name.replace(/_[0-9]+$/g, "") === p1 && ( typeof sources[s].alias == 'undefined' || sources[s].name.startsWith('ads_') || sources[s].name === "events_*" ) ) ) )
             ||
             (p2 !== "" && (sources[s].alias === p2 || (sources[s].name.replace(/_[0-9]+$/g, "") === p2 && (typeof sources[s].alias == 'undefined' || sources[s].name.startsWith('ads_') || sources[s].name === "events_*" || sources[s].name.endsWith("Producer")) ) ) && sources[s].schema === p1)
