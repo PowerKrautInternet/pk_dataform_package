7295d5772d01c7981fb086b8399d16f9bc6fc274
Stijn Holtus <stijn@powerkraut.nl>
2025-10-21 10:36:47 +0200

    Update stg_ga4_mappings_targets.js

--- queries/df_staging_views/stg_ga4_mappings_targets.js
+++ queries/df_staging_views/stg_ga4_mappings_targets.js
@@ -82,11 +82,11 @@ SELECT
     FROM ${ref("df_staging_tables", "stg_ga4_events_sessies")} events_sessies
     
     ${join("left join","df_googlesheets_tables","gs_ga4_standaard_events", `
-    AS standaard_event ON TRIM(events_sessies.event_name) = TRIM(standaard_event.event_name) AND 
-    events_sessies.account = standaard_event.account
+    AS standaard_event ON TRIM(events_sessies.event_name) = TRIM(standaard_event.event_name)
     ${join_on_account(
             {name: "events_*"}, 
-            {schema: "googleSheets", name: "gs_ga4_standaard_events"}
+            {schema: "googleSheets", name: "gs_ga4_standaard_events"},
+            " AND events_sessies.account = standaard_event.account"
     )}`)}
     ${join("left join","df_googlesheets_tables","gs_conversie_mapping", "AS gs_mapping ON TRIM(events_sessies.event_name) = TRIM(gs_mapping.event_name) AND events_sessies.account = gs_mapping.account")}
     ${join("full Outer Join","df_staging_views", "stg_pivot_targets", "AS targets ON 1=0")}
