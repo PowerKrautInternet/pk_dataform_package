bf53d941a44f2ae0a42c927d08b6fe9856c7ca70
PowerkrautRobby <138665624+PowerkrautRobby@users.noreply.github.com>
2025-10-21 14:28:48 +0200

    Add submission_id_otm to GA4 marketing query

--- queries/df_staging_views/stg_ga4_marketing_kanalen_combined.js
+++ queries/df_staging_views/stg_ga4_marketing_kanalen_combined.js
@@ -13,9 +13,8 @@ SELECT
     session_geo_city,
     session_source_medium,
     user_pseudo_id
+    submission_id_otm,
     ${ifSource("gs_activecampaign_ga4_mapping",", ac_name")}
-    ${ifSource("stg_otm_aggregated", ", submission_id_otm")}
-    ,
     account ${ifSource("stg_hubspot_workflowstats",", hs_workflow_name, edm_name")}
     ),
     ${ifSource("stg_marketingkanalen_combined", "marketing_kanalen.* EXCEPT(bron, campaign_name, record_date, campaign_id, ad_group_id, ad_group_name, merk, account")}
