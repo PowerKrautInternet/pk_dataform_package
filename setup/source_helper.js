1b015dd01034019fa545d7347eb47c0ebb760dcd
PowerKrautWouterFloors <138665492+PowerKrautWouterFloors@users.noreply.github.com>
2025-11-11 11:41:24 +0100

    Update source_helper.js

--- setup/source_helper.js
+++ setup/source_helper.js
@@ -1,4 +1,3 @@
-
 class SourceHelper {
   constructor(defaultDatabase, defaultSchema = 'rawdata') {
     this.defaultDatabase = defaultDatabase;
@@ -6,8 +5,8 @@ class SourceHelper {
     this.sources = [];
   }
 
-  addSource(name, table, schema = this.defaultSchema, database = this.defaultDatabase) {
-    const source = { name, table, schema, database };
+  addSource(name, schema = this.defaultSchema, database = this.defaultDatabase) {
+    const source = { name, schema, database };
     this.sources.push(source);
     return source;
   }
@@ -16,9 +15,39 @@ class SourceHelper {
     return this.sources.find(src => src.name === name);
   }
 
+  setSources(varSource) {
+    this.sources = [];
+
+    for (const s in varSource) {
+      const v = varSource[s];
+
+      // Verwerkt de 'publishers'-array
+      v.publishers = (v.publishers ?? [])
+        .filter(publisher => !!publisher.name)
+        .flatMap(publisher => {
+          const names = Array.isArray(publisher.name)
+            ? publisher.name
+            : [publisher.name];
+
+          return names.map(name => ({
+            name,
+            recency: publisher.recency ?? true,
+            freshnessDays: publisher.freshnessDays ?? v.freshnessDays ?? 1,
+          }));
+        });
+
+      // Interne flags
+      v.noSuffix = true;
+      v.declaredSource = true;
+
+      this.sources.push(v);
+    }
+  }
+
   listSources() {
     return this.sources;
   }
 }
 
 module.exports = SourceHelper;
+
