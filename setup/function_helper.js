caeed76ac7fe8d3316e6b6877002f83b53c8f98f
Stijn Holtus <stijn@powerkraut.nl>
2025-11-17 16:17:23 +0100

    Update function_helper.js

--- setup/function_helper.js
+++ setup/function_helper.js
@@ -6,35 +6,7 @@ class FunctionObject {
         this.function = config.function ?? null;
         this.vars = config.vars ?? {}
         this.return_type = config.return_type ?? "STRING"
-        this.type = config.function_type ?? "javascript"
         this.sql = this;
-    }
-
-    set function_type(function_type) {
-        this.type = function_type.toLowerCase();
-    }
-
-    get language() {
-        switch (this.type) {
-            case "javascript":
-                return ` LANGUAGE js AS R""" `;
-            case "sql":
-                return " AS ( ";
-            default:
-                throw new Error(`Switch error! setup/language/${this.name}`);
-        }
-    }
-
-    get return() {
-        switch (this.type) {
-            case "javascript":
-                return ` return ${this.sql_object.name}(${this.sql_object.params});
-                        """`
-            case "sql":
-                return ` ); `
-            default:
-                throw new Error(`Switch error! setup/return/${this.name}`);
-        }
     }
 
     get sql() {
@@ -46,14 +18,12 @@ class FunctionObject {
     }
 
     set sql(the_function_object) {
-        this.sql_object = the_function_object;
         this.sqlForFunction = `CREATE OR REPLACE FUNCTION 
         \`${the_function_object.database}.${the_function_object.schema}.${the_function_object.name}\` (${the_function_object.vars}) 
-        RETURNS ${the_function_object.return_type} 
-        ${this.language}
-        ${the_function_object.function}
-        ${this.return}`
-
+        RETURNS ${the_function_object.return_type} LANGUAGE js AS R""" 
+            ${the_function_object.function}
+            return ${the_function_object.name}(${the_function_object.params});
+        """`
     }
 
     // Helper method to format the parameters for the function signature
@@ -62,16 +32,13 @@ class FunctionObject {
             .map(([param, type]) => `${param} ${type}`)
             .join(", ");
     }
-
     set vars(parameterVars) {
         this.varsForFunction = parameterVars
     }
-
     // Helper method to format the actual parameters in the return statement
     get params() {
         return Object.keys(this.varsForFunction).join(", ");
     }
-
     get source(){
         return {
             config: {
@@ -83,5 +50,4 @@ class FunctionObject {
         }
     }
 }
-
 module.exports = FunctionObject
