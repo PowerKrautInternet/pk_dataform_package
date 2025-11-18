14542137b738c283693df2f4017a72553d6b2933
Stijn Holtus <stijn@powerkraut.nl>
2025-11-18 09:42:20 +0100

    object handling in ref added
    Signed-off-by: Stijn Holtus <stijn@powerkraut.nl>

--- sources/index.js
+++ sources/index.js
@@ -190,8 +190,24 @@ function ref(p1, p2, ifSource = false, dependant = true) {
     let sources = getSources();
     let ref = [];
     let NrFound = 0;
-    for(let s in sources) {
-        if( //if the ref has only one parameter it has to be the name, when there are 2 parameter the second wil be the name. (name is interchangable with alias)
+    for(let s in sources) {    /**
+     * @brief New ref implementation. this is based on only p1.
+     */
+        if(typeof p1 === "object"){
+            let database = p1.database ?? dataform.projectConfig.defaultDatabase
+            let schema = p1.schema
+            let name = p1.name ?? null
+            if(name == null) throw new Error("No name found in ref function")
+            let type = p1.type
+            let dependencie = p1.dependencie ?? type === "function"
+            dependencie ? refs.push({
+                "name": name,
+                "schema": schema,
+                "database": database
+            }) : null
+            return `\`${database}.${schema}.${name}\``
+        } else
+        if ( //if the ref has only one parameter it has to be the name, when there are 2 parameter the second wil be the name. (name is interchangable with alias)
             typeof sources[s].name != 'undefined' &&
             ((p2 === "" && (sources[s].alias === p1 || (sources[s].name.replace(/_[0-9]+$/g, "") === p1 && ( typeof sources[s].alias == 'undefined' || sources[s].name.startsWith('ads_') || sources[s].name === "events_*" ) ) ) )
             ||
