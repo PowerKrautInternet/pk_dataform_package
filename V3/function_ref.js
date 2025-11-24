283e8792a886ed5dc844f0ece00da1ad0e91dca5
Stijn Holtus <stijn@powerkraut.nl>
2025-11-24 11:25:00 +0100

    testing function reffering
    Signed-off-by: Stijn Holtus <stijn@powerkraut.nl>

--- V3/function_ref.js
+++ V3/function_ref.js
@@ -9,6 +9,7 @@ const pkg = {
         if (typeof this.handler !== "function") {
             throw new Error("No handler set");
         }
+        this.handler("some_table2",{ type: "table", schema: "test_tables"}).query("SELECT * FROM `test`")
         return this.handler("some_table",{ type: "table", schema: "test_tables"}).query("SELECT * FROM `test`");
     }
 };
