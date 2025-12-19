e0ab9c152847f3e268b23e99d515fcf869402bb7
PowerKrautWouterFloors <138665492+PowerKrautWouterFloors@users.noreply.github.com>
2025-12-19 11:35:13 +0100

    Update sam_occasion.js

--- queries/df_rawdata_views/sam_occasion.js
+++ queries/df_rawdata_views/sam_occasion.js
@@ -4,27 +4,27 @@ let pk = require("../../sources")
 let ref = pk.ref
 let query = `
 SELECT
-        JSON_VALUE(PAYLOAD, '$.OCCASIONID') as OCCASION_OCCASIONID,
-        JSON_VALUE(PAYLOAD, '$.MERKNAAM') as OCCASION_MERKNAAM,
-        JSON_VALUE(PAYLOAD, '$.MODELNAAM') as OCCASION_MODELNAAM,
-        JSON_VALUE(PAYLOAD, '$.UITVOERINGNAAM') as OCCASION_UITVOERINGNAAM,
-        JSON_VALUE(PAYLOAD, '$.KILOMETERSTAND') as OCCASION_KILOMETERSTAND,
-        JSON_VALUE(PAYLOAD, '$.KENTEKEN') as OCCASION_KENTEKEN,
-        JSON_VALUE(PAYLOAD, '$.CHASSISNR') as OCCASION_CHASSISNR,
-        JSON_VALUE(PAYLOAD, '$.DATUMDEEL1') as OCCASION_DATUMDEEL1,
-        JSON_VALUE(PAYLOAD, '$.BOUWJAAR') as OCCASION_BOUWJAAR,
-        JSON_VALUE(PAYLOAD, '$.DTCMEDIA_CRM_ID') as OCCASION_DTCMEDIA_CRM_ID
-FROM (
-    SELECT MAX(SCHEMA), PRIMARYFIELDHASH, MAX(PAYLOAD) as PAYLOAD, MAX(ACTION) as ACTION, MAX(RECEIVEDON) as RECEIVEDON
-      FROM
-         ${ref("samDataProducer_lasttransaction")} as first
-         WHERE SCHEMA IN (SELECT schema FROM ${ref("sam_table_hashes")} WHERE tableName = 'OCCASION')
-         AND RECEIVEDON = (
-            SELECT MAX(RECEIVEDON)
-            FROM ${ref("samDataProducer_lasttransaction")} as second
-            WHERE first.PRIMARYFIELDHASH = second.PRIMARYFIELDHASH and first.SCHEMA = second.SCHEMA)
-        GROUP BY PRIMARYFIELDHASH
-)
+  JSON_VALUE(PAYLOAD, '$.occasionid') AS OCCASION_OCCASIONID,
+  JSON_VALUE(PAYLOAD, '$.response.kenteken') AS OCCASION_KENTEKEN,
+  JSON_VALUE(PAYLOAD, '$.response.merknaam') AS OCCASION_MERKNAAM,
+  JSON_VALUE(PAYLOAD, '$.response.modelnaam') AS OCCASION_MODELNAAM,
+  JSON_VALUE(PAYLOAD, '$.response.uitvoeringnaam') AS OCCASION_UITVOERINGNAAM,
+  JSON_VALUE(PAYLOAD, '$.response.kilometerstand') AS OCCASION_KILOMETERSTAND,
+  JSON_VALUE(PAYLOAD, '$.response.bouwjaar') AS OCCASION_BOUWJAAR,
+  JSON_VALUE(PAYLOAD, '$.response.datumdeel1') AS OCCASION_DATUMDEEL1,
+  JSON_VALUE(PAYLOAD, '$.response.chassisnr') AS OCCASION_CHASSISNR,
+  JSON_VALUE(PAYLOAD, '$.response.kleur') AS OCCASION_KLEUR,
+  JSON_VALUE(PAYLOAD, '$.response.brandstof') AS OCCASION_BRANDSTOF,
+  JSON_VALUE(PAYLOAD, '$.response.automaat') AS OCCASION_AUTOMAAT,
+  JSON_VALUE(PAYLOAD, '$.response.locatie') AS OCCASION_LOCATIE,
+  JSON_VALUE(PAYLOAD, '$.response.occasionstatusid') AS OCCASION_STATUSID,
+  JSON_VALUE(PAYLOAD, '$.response.verkoopprijs') AS OCCASION_VERKOOPPRIJS,
+  JSON_VALUE(PAYLOAD, '$.response.nieuwprijs') AS OCCASION_NIEUWPRIJS,
+  JSON_VALUE(PAYLOAD, '$.response.mutatiedatum') AS OCCASION_MUTATIEDATUM,
+  JSON_VALUE(PAYLOAD, '$.dtcmedia_crm_id') AS OCCASION_DTCMEDIA_CRM_ID
+FROM 
+      ${ref("odbcDataProducer_lasttransaction")}
+      WHERE PUBLISHER = "SAMOccasionPublisher"
 
 `
 let refs = pk.getRefs()
