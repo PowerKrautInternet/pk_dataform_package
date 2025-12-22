a96f04d8a88d4a5ff0bb0ec46066a004eec40670
PowerKrautWouterFloors <138665492+PowerKrautWouterFloors@users.noreply.github.com>
2025-12-22 10:27:39 +0100

    Update sam_offerte_status.js

--- queries/df_rawdata_views/sam_offerte_status.js
+++ queries/df_rawdata_views/sam_offerte_status.js
@@ -4,10 +4,10 @@ let pk = require("../../sources")
 let ref = pk.ref
 let query = `
 SELECT
-  JSON_VALUE(PAYLOAD, '$.offertestatusid') AS OFFERTE_STATUS_ID,
-  JSON_VALUE(PAYLOAD, '$.response.actief') AS OFFERTE_STATUS_ACTIEF,
-  JSON_VALUE(PAYLOAD, '$.response.omschrijving') AS OFFERTE_STATUS_OMSCHRIJVING,
-  JSON_VALUE(PAYLOAD, '$.dtcmedia_crm_id') AS OFFERTE_STATUS_DTCMEDIA_CRM_ID
+  JSON_VALUE(PAYLOAD, '$.offertestatusid') AS OFFERTESTATUS_OFFERTESTATUSID,
+  JSON_VALUE(PAYLOAD, '$.response.actief') AS OFFERTESTATUS_ACTIEF,
+  JSON_VALUE(PAYLOAD, '$.response.omschrijving') AS OFFERTESTATUS_OMSCHRIJVING,
+  JSON_VALUE(PAYLOAD, '$.dtcmedia_crm_id') AS OFFERTESTATUS_DTCMEDIA_CRM_ID
 FROM
       ${ref("odbcDataProducer_lasttransaction")}
       WHERE PUBLISHER = "SAMOfferteStatusPublisher"
