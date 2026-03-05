68b676adb57ca8ba77bd526f439bda9953b97058
PowerKrautWouterFloors <138665492+PowerKrautWouterFloors@users.noreply.github.com>
2026-03-05 12:51:08 +0100

    Update stg_ga4_marketing_kanalen_combined.js

--- queries/df_staging_views/stg_ga4_marketing_kanalen_combined.js
+++ queries/df_staging_views/stg_ga4_marketing_kanalen_combined.js
@@ -68,7 +68,7 @@ SELECT
     ${ifNull([
         "ga4.event_date",
         ifSource("stg_marketingkanalen_combined", "marketing_kanalen.record_date"),
-        ifSource("stg_lef_leads_agg", "CAST(lef.record_date AS DATE)"),
+        ifSource("stg_lef_leads_agg", "lef.record_date"),
         ifSource("stg_marketingdashboard_searchconsole", "searchconsole.data_date"),
         ifSource("stg_syntec_leads_orders_combined", "syntec.record_date"),
         ifSource("stg_activecampaign_ga4_sheets", "ac.record_datum"),
