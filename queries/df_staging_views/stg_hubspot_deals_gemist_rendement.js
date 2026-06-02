3bf27319545b6a0d5f6f52c478df27be4df8de8d
PowerKrautWouterFloors <138665492+PowerKrautWouterFloors@users.noreply.github.com>
2026-06-02 14:20:07 +0200

    Create stg_hubspot_deals_gemist_rendement.js

---
+++ queries/df_staging_views/stg_hubspot_deals_gemist_rendement.js
@@ -0,0 +1,25 @@
+/*config*/
+let pk = require("../../sources")
+let ref = pk.ref
+let query = `
+
+SELECT 
+    deal_source,
+    deal_id,
+    deal_status,
+    DATE(TIMESTAMP_MILLIS(deal_start_datum)) AS deal_start_datum,
+    DATE(TIMESTAMP_MILLIS(deal_eind_datum)) AS deal_eind_datum,
+    deal_afsluit_reden,
+    deal_vestiging,
+    gewenst_soort_auto,
+    gewenst_merk,
+    gewenst_model
+
+FROM ${ref("df_datamart_tables", "dm_hubspot_deals")}
+
+WHERE deal_status IN ("Gemiste Lead", "Gemist Salestraject")
+AND (lead_type != "Aftersales" OR lead_type IS NULL) 
+
+`
+let refs = pk.getRefs()
+module.exports = {query, refs}
