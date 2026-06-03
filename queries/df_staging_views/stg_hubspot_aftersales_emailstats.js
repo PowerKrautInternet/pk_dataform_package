2927ccf4ef376604009f33f80523bd2a9c24f365
PowerKrautWouterFloors <138665492+PowerKrautWouterFloors@users.noreply.github.com>
2026-06-03 09:31:57 +0200

    Create stg_hubspot_aftersales_emailstats.js

---
+++ queries/df_staging_views/stg_hubspot_aftersales_emailstats.js
@@ -0,0 +1,39 @@
+/*config*/
+let pk = require("../../sources")
+let ref = pk.ref
+let query = `
+
+
+SELECT 
+datum_bericht,
+verstuurd_bericht,
+email,
+kenteken,
+werkplaats_vestiging,
+FROM(
+  SELECT
+    CAST(TIMESTAMP_MILLIS(CAST(datum_bericht AS INT64)) AS DATE) AS datum_bericht,
+    flows.workflow_name AS verstuurd_bericht,
+    berijder_email AS email,
+    kenteken,
+    werkplaats_vestiging,
+    workflow_id,
+
+  FROM(
+    SELECT 
+    *
+    FROM
+    ${ref("df_staging_views","stg_hubspotworkflows_voertuigen")} 
+  ) msg
+
+  LEFT JOIN
+    ${ref("googleSheets","gs_workflow_mapping")} flows
+  ON
+    msg.workflow_id = flows.workflow_id
+  AND
+    flows.type = "Aftersales"
+  WHERE workflow_id <> "" AND workflow_id is not null
+) voertuig
+`
+let refs = pk.getRefs()
+module.exports = {query, refs}
